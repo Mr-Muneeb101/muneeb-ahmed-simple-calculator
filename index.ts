7c7d474f3133bb3d080a8bda3ad03d7e9f753b10
Mr-Muneeb101 <160252527+Mr-Muneeb101@users.noreply.github.com>
2024-07-20 12:09:16 -0700

    Add files via upload

---
+++ index.ts
@@ -0,0 +1,37 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+async function calculate() {
+    while (true) {
+        let answers = await inquirer.prompt([
+            {
+                name: "num1",
+                type: "number",
+                message: "Enter the first number",
+            },
+            {
+                name: "num2",
+                type: "number",
+                message: "Enter the Second number",
+            },
+            {
+                name: "Operation",
+                type: "list",
+                message: "Select any operation from below",
+                choices: ["+", "-", "*", "/", "Back"],
+            }
+        ])
+        if (answers.Operation === '+') {
+            console.log(` ${answers.num1} + ${answers.num2} = ${answers.num1 + answers.num2} `);
+        } else if ((answers.Operation === '-')) {
+            console.log(` ${answers.num1} - ${answers.num2} = ${answers.num1 - answers.num2} `);
+        } else if ((answers.Operation === '/')) {
+            console.log(` ${answers.num1} / ${answers.num2} = ${answers.num1 / answers.num2} `);
+        } else if ((answers.Operation === '*')) {
+            console.log(` ${answers.num1} * ${answers.num2} = ${answers.num1 * answers.num2} `);
+        } else if ((answers.Operation === 'Back')) {
+            return false;
+        }
+    }
+}
+
+calculate();
